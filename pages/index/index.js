18522294c58ca49ec25650f4e00f48425f273e2d
wangyu <www_wayne_001@163.com>
2018-02-09 18:03:35 +0800

    请求交互

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,23 +7,81 @@ Page({
     queryNumber: -1,
     isShowResult: false,
     text: '',
-    isLoading:false
+    isLoading: false
   },
-  
+
   onLoad: function () {
-    
+
   },
 
-  query: function(){
-    this.setData({ isShowResult:true});
+  bindInput: function (e) {
+    this.setData({
+      queryNumber: e.detail.value
+    })
   },
 
-  create: function(){
+  query: function () {
+    if (this.data.queryNumber <= 0) {
+      wx.showToast({
+        title: '查询码不正确！',
+        icon: 'none'
+      });
+      return
+    }
 
+    var that = this;
+    wx.request({
+      url: 'https://wycode.cn/web/api/public/clipboard/query',
+      data: { 'id': this.data.queryNumber },
+      success: function (res) {
+        if (!res.data.success || res.data.data == null) {
+          wx.showToast({
+            title: '查询码不正确！',
+            icon: 'none'
+          });
+        } else {
+          that.setData({
+            isShowResult: true,
+            queryNumber: res.data.data.id,
+            text: res.data.data.content
+          });
+        }
+      },
+      fail: function (res) {
+        wx.showToast({
+          title: res,
+          icon: 'none'
+        });
+      }
+    });
+
+  },
+
+  create: function () {
+    var that = this;
+    wx.request({
+      url: 'https://wycode.cn/web/api/public/clipboard/create',
+      method: 'POST',
+      success: function (res) {
+
+        that.setData({
+          isShowResult: true,
+          queryNumber: res.data.data.id,
+          text: res.data.data.content
+        });
+
+      },
+      fail: function (res) {
+        wx.showToast({
+          title: res.errMsg,
+          icon: 'none'
+        });
+      }
+    });
   },
 
   save: function () {
 
   }
-  
+
 })
